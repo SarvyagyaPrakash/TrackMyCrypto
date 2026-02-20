27b3d31668dbf23a55b1481fc8b656fc1834f005
Sarvyagya Prakash <prakash.sarvyagya@gmail.com>
2026-02-20 18:04:37 +0530

    Initial Commit

---
+++ Cryptocurrency App/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Cryptocurrency App
+//
+//  Created by Sarvyagya Prakash on 20/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Cryptocurrency AppTests/Cryptocurrency_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Cryptocurrency_AppTests.swift
+//  Cryptocurrency AppTests
+//
+//  Created by Sarvyagya Prakash on 20/02/26.
+//
+
+import Testing
+
+struct Cryptocurrency_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
